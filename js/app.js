afc9c34465a2add4a861de8d999fcc7dd1f3b11f
Nataliesmyth <nataliesmyth11@gmail.com>
2021-07-12 20:16:20 -0400

    hero updates

--- js/app.js
+++ js/app.js
@@ -4,7 +4,7 @@ let readout = document.querySelector('.readout');
 
 let count = 0
 
-let text = ["Software Engineer", "Programmer", "Handstand Efficianato", "Full Stack Web Developer", "George R.R. Martin fan girl", "CSS enthusiast" ];
+let text = ["Software Engineer", "Programmer", "Handstand Connoisseur", "Full Stack Web Developer", "George R.R. Martin fan girl", "CSS enthusiast" ];
 setInterval(e => {
   let randomNumber = Math.floor(Math.random()*11);
   count = (count + randomNumber) % text.length;
