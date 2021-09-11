309faa4cd82a9d2b0c35639391c00e94101aaf3c
Nataliesmyth <nataliesmyth11@gmail.com>
2021-09-11 16:18:13 -0400

    scroll and collapse elements

--- js/app.js
+++ js/app.js
@@ -1,4 +1,17 @@
-console.log('sanity check')
+// console.log('sanity check')
+
+
+var scrollSpy = new bootstrap.ScrollSpy(document.body, {
+  target: '#navbarScroll'
+})
+
+
+// var waypoint = new Waypoint({
+//   element: document.getElementById('context-example'),
+//   handler: function() {
+//     notify('Basic waypoint triggered')
+//   }
+// })
 
 let readout = document.querySelector('.readout');
 
@@ -12,36 +25,13 @@ setInterval(e => {
   readout.innerHTML = `<b>${newText}</b>`
 }, 2000);
 
-$(function() {
-  
-  console.log($);
 
-  $('.nav a').on('click', function(){
-    $('.btn-navbar').click();
-    $('.navbar-toggle').click()
-});
 
-fadeInUp('#about-me-blurb', 370);
-fadeInUp('.all-steps', 855);
-fadeInUp('.work-flow-text', 855);
-fadeInUp("#a-few-short-miles", 1280);
-fadeInUp("#few-short-miles-preview", 1280);
-fadeInUp("#within-reach", 1860);
-fadeInUp("#within-reach-preview", 1860);
-fadeInUp("#the-best-hour", 2430);
-fadeInUp("#best-hour-preview", 2430);
-fadeInUp(".contact-text", 3135);
-fadeInUp(".icon-container", 3135);
-fadeInUp(".icon-container", 3135);
-fadeInUp('#copyright', 3135);
 
-function fadeInUp(element, height){
-  $(window).scroll(function() { 
-      if ($(this).scrollTop() > height) { 
-          $(element).css('visibility','visible');   
-          $(element).addClass('animated fadeInUp');  
-      } 
-  });
-};
 
-});
+
+
+
+
+// console.log(waypoint.element);
+
